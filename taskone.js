e940fcb07cec5254a0fe5a871b3c83435f675a8b
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-08-11 20:50:29 +0500

    Bug fixed

--- taskone.js
+++ taskone.js
@@ -2,26 +2,36 @@ function zeroEnd(numbers) {
   
   let new_arr_zeroes = [];
   let new_arr_values = [];
-  
+  let flag = false;
   for(let i = 0; i < numbers.length; i++) {
   	 
   	 if(Number(numbers[i]) == 0) {
   	 
   	 	new_arr_zeroes.push(numbers[i]);
   	 
-  	 }else {
+  	 }
+
+     if(numbers[i] > 0) {
   	 
   	 	new_arr_values.push(numbers[i]);
   	 
   	 }
+
+     else {
+      flag = true;
+     }
   
   }
+
+   if(flag) {
+      console.log("Eslatma, manfiy raqamlar hisobga olinmaydi !");
+   }
   
   return new_arr_values.concat(new_arr_zeroes).join(""); 
 
 }
 
-// let numbers = [1,3,0,5,0,7,0];
+// let numbers = [1,-3,0,5,0,7,0];
 
 // let result = zeroEnd(numbers);
 
